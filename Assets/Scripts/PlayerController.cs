688c8d3ce6910e8f1e4ab0bd48e3f136cccdba82
Aynur19 <nasybullin.aynur@yandex.ru>
2021-06-23 13:36:47 +0300

    add player focusing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour
 {
+	public Interactable focus;
 	public LayerMask movementMask;
 
 	private Camera mainCamera;
@@ -25,6 +26,8 @@ public class PlayerController : MonoBehaviour
 			if (Physics.Raycast(ray, out hit, 100, movementMask))
 			{
 				motor.MoveToPoint(hit.point);
+
+				RemoveFocus();
 			}
 		}
 
@@ -35,7 +38,40 @@ public class PlayerController : MonoBehaviour
 
 			if (Physics.Raycast(ray, out hit, 100))
 			{
+				var interactable = hit.collider.GetComponent<Interactable>();
+
+				if (interactable != null)
+				{
+					SetFocus(interactable);
+				}
 			}
 		}
+	}
+
+	private void SetFocus(Interactable newFocus)
+	{
+		if (newFocus != focus)
+		{
+			if (focus != null)
+			{
+				focus.OnDefocused();
+			}
+
+			focus = newFocus;
+			motor.FollowTarget(focus);
+		}
+
+		focus.OnFocused(transform);
+	}
+
+	private void RemoveFocus()
+	{
+		if (focus != null)
+		{
+			focus.OnDefocused();
+		}
+
+		focus = null;
+		motor.StopFollowingTarget();
 	}
 }
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -4,6 +4,7 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMotor : MonoBehaviour
 {
+	private Transform target;
 	private NavMeshAgent agent;
 
 	private void Start()
@@ -11,8 +12,40 @@ public class PlayerMotor : MonoBehaviour
 		agent = GetComponent<NavMeshAgent>();
 	}
 
+	private void Update()
+	{
+		if (target != null)
+		{
+			agent.SetDestination(target.transform.position);
+			FaceTarget();
+		}
+	}
+
 	public void MoveToPoint(Vector3 point)
 	{
 		agent.SetDestination(point);
+	}
+
+	public void FollowTarget(Interactable newTarget)
+	{
+		agent.stoppingDistance = newTarget.radius * 0.8f;
+		agent.updateRotation = false;
+
+		target = newTarget.interactionTransform;
+	}
+
+	public void StopFollowingTarget()
+	{
+		agent.stoppingDistance = 0f;
+		agent.updateRotation = true;
+
+		target = null;
+	}
+
+	private void FaceTarget()
+	{
+		var direction = (target.position - transform.position).normalized;
+		var lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+		transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
 	}
 }
